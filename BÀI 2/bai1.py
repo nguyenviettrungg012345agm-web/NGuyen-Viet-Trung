303c753de9fd90f36013d4dc03c71cf1978c59d2
nguyenviettrungg012345agm-web <nguyenviettrungg012345agm@gmail.com>
2025-12-10 07:43:03 +0700

    Modify print statements in bai1.py
    Updated print statements to include new user information.

--- BÀI 2/bai1.py
+++ BÀI 2/bai1.py
@@ -1,9 +1,10 @@
-print("hello world")
-print("HoDucToan_MSSV:245752021610012_KTDKvaTDH")
+print("Nguyen Viet Trung")
+print("_Msv:245752021610157_KTDKvaTDH")
 
 n1 = int(input("enter n1 value"))
 n2 = int(input("enter n2 value"))
 if n1 > n2:
     print("n1 is big")
 else:
+
     print("n2 is big")
