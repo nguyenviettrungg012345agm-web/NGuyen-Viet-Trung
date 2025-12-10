39935bc4dc48a2006c41e6dc04849d0fcd2ed6b7
nguyenviettrungg012345agm-web <nguyenviettrungg012345agm@gmail.com>
2025-12-10 08:16:21 +0700

    Update BAI 1.py

--- BÀI 3/BAI 1.py
+++ BÀI 3/BAI 1.py
@@ -1,5 +1,9 @@
+print("Nguyen Viet Trung")
+print(_Msv:245752021610157_KTDKvaTDH")
+
 a=int(input("nhap vao gia tri a: "))
 b=int(input("nhap vao gia tri b: "))
 def sum(a,b):
  print("sum=" +str(a+b))
 sum(a,b)
+
